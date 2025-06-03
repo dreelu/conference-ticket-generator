10715d7ad64ad6413d187907601fe4abd41c6f06
eb <andre.luis8173@gmail.com>
2025-06-03 17:38:52 -0300

    Beta 1.0.2

--- src/script.js
+++ src/script.js
@@ -1,10 +1,19 @@
+let generalError = false
 let fileInput = document.querySelector("input#inputFile")
 let imgURL = null
 
 fileInput.addEventListener("change", function(event) {
     const file = event.target.files[0]
-
+    const maxSize = 500 * 1024
      if (!file) return
+
+    if (file.size > maxSize) {
+        const fileWarning = document.querySelector("small#fileWarning")
+        fileWarning.innerHTML = 'File too large. Please upload a photo under 500KB'
+        fileWarning.style.color = 'hsl(7, 88%, 67%)'
+        generalError = true
+
+    }
 
     imgURL = URL.createObjectURL(file)
 })
@@ -28,6 +37,28 @@ function generateTicket() {
 
     let codeOutput = document.querySelector("strong#codeOutput")
     let code = ''
+
+    let userimageError = false
+    let usernameError = false
+    let useremailError = false
+    let usergithubError = false
+
+    if (fileInput.value == '') {
+        generalError = true
+    }
+
+    if (useremail instanceof HTMLInputElement) {
+        if (useremail.value === '' || !useremail.checkValidity()) {
+            document.querySelector("div#emailWarning").classList.remove('hidden')
+            useremail.style.borderColor = 'hsl(7, 88%, 67%)'
+            generalError = true
+        } else {
+            document.querySelector("div#emailWarning").classList.add('hidden')
+            useremail.style.borderColor = 'hsl(245, 15%, 58%)'
+        }
+    }
+
+    if (generalError) return
 
     for (let i = 0; i < 6; i++) {
         let numeral = '0123456789'
